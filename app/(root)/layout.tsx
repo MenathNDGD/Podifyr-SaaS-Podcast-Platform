05bcf6a9be3e5f7d7b667ccb6f96ea851337e32c
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-15 21:38:27 +0530

    Refactor layout component to include mobile navigation and right sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/LeftSidebar";
+import MobileNav from "@/components/MobileNav";
+import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 
 export default function RootLayout({
@@ -11,19 +13,24 @@ export default function RootLayout({
       <main className="relative flex bg-black-3">
         <LeftSidebar />
 
-        <section>
-          <div>
-            <div>
-              <Image />
-              {/* TODO: Mobile Nav */}
+        <section className="flex flex-col flex-1 min-h-screen px-4 sm:px-14">
+          <div className="flex flex-col w-full max-w-5xl mx-auto max-sm:px-4">
+            <div className="flex items-center justify-between h-16 md:hidden">
+              <Image
+                src={"/icons/logo.png"}
+                width={30}
+                height={30}
+                alt="Podifyr"
+              />
+              <MobileNav />
             </div>
-            <div>
+            <div className="flex flex-col md:pb-14">
               {/* TODO: Wrap with Toaster */}
               {children}
             </div>
           </div>
         </section>
-        {/* TODO: Right Sidebar */}
+        <RightSidebar />
       </main>
     </div>
   );

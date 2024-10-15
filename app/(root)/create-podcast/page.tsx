bc1d02cd896131fccbd76b4d6203f512b5bba324
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-15 21:40:27 +0530

    Refactor CreatePodcast page to include a bold heading for podcast creation

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const CreatePodcast = () => {
-  return <div>CreatePodcast</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-20 text-white-1">
+        Create Your Own Podcasts
+      </h1>
+    </div>
+  );
 };
 
 export default CreatePodcast;

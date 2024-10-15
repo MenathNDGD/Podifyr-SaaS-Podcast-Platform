dfc3877c6777185d487805b0a70658b8a5a0820d
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-16 00:03:31 +0530

    Refactor Home page to include trending podcasts from Convex API

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import React from "react";
 import PodcastCard from "@/components/PodcastCard";
 import { podcastData } from "@/constants";
-import React from "react";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
+  
   return (
     <div className="flex flex-col mt-9 gap-9 md:overflow-hidden">
       <section className="flex flex-col gap-5">
         <h1 className="font-bold text-20 text-white-1">
           Trending Podcasts Over The Globe
         </h1>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+        </div>
         <div className="podcast_grid">
           {podcastData.map(({ id, title, description, imgURL }) => (
             <PodcastCard

130f56aa80863fe54c5b11ab73da230a2a46181c
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-15 11:08:30 +0530

    Refactor layout.tsx to add LeftSidebar and improve mobile navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,28 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-      <main>
-        {/* TODO: Left Sidebar */}
-        {children}
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+
+        <section>
+          <div>
+            <div>
+              <Image />
+              {/* TODO: Mobile Nav */}
+            </div>
+            <div>
+              {/* TODO: Wrap with Toaster */}
+              {children}
+            </div>
+          </div>
+        </section>
         {/* TODO: Right Sidebar */}
       </main>
     </div>

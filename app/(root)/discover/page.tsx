c82396a06e36f7efaa1de4ceb026c697fb670bfb
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-15 21:39:27 +0530

    Refactor Discover page to include a bold heading for podcast discovery

--- app/(root)/discover/page.tsx
+++ app/(root)/discover/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Discover = () => {
-  return <div>Discover</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-20 text-white-1">
+        Discover Your Next Favorite Podcast
+      </h1>
+    </div>
+  );
 };
 
 export default Discover;

dcc6cfd38c04cda948c3c4ad6bbe4cd08ce109e0
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-17 01:27:37 +0530

    Refactor PodcastCard component and add click event for increasing views

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,22 +1,29 @@
-import Image from "next/image";
 import React from "react";
+import { PodcastCardProps } from "@/types";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const PodcastCard = ({
   podcastId,
   title,
   description,
-  imgURL,
-}: {
-  podcastId: number;
-  title: string;
-  description: string;
-  imgURL: string;
-}) => {
+  imgUrl,
+}: PodcastCardProps) => {
+  const router = useRouter();
+
+  const handleViews = () => {
+    // TODO: increase the views of the podcast
+
+    router.push(`/podcasts/${podcastId}`, {
+      scroll: true,
+    });
+  };
+
   return (
-    <div className="cursor-pointer">
+    <div className="cursor-pointer" onClick={handleViews}>
       <figure className="flex flex-col gap-2">
         <Image
-          src={imgURL}
+          src={imgUrl}
           width={174}
           height={174}
           alt={title}

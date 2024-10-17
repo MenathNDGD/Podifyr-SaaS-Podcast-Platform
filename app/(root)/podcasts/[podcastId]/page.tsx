6e16843547f4170072da595adaa6fd6cd373ac32
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-18 02:53:49 +0530

    Refactor podcast details page to add podcast player and display similar podcasts

--- app/(root)/podcasts/[podcastId]/page.tsx
+++ app/(root)/podcasts/[podcastId]/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import EmptyState from "@/components/EmptyState";
+import LoaderSpinner from "@/components/LoaderSpinner";
+import PodcastCard from "@/components/PodcastCard";
+import PodcastDetailPlayer from "@/components/PodcastDetailPlayer";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 import Image from "next/image";
 
@@ -10,7 +15,17 @@ const PodcastDetails = ({
 }: {
   params: { podcastId: Id<"podcasts"> };
 }) => {
+  const { user } = useUser();
+
   const podcast = useQuery(api.podcasts.getPodcastById, { podcastId });
+
+  const similarPodcasts = useQuery(api.podcasts.getPodcastByVoiceType, {
+    podcastId,
+  });
+
+  const isOwner = user?.id === podcast?.authorId;
+
+  if (!similarPodcasts || !podcast) return <LoaderSpinner />;
 
   return (
     <section className="flex flex-col w-full">
@@ -28,6 +43,56 @@ const PodcastDetails = ({
           <h2 className="font-bold text-16 text-white-1">{podcast?.views}</h2>
         </figure>
       </header>
+      <PodcastDetailPlayer
+        isOwner={isOwner}
+        podcastId={podcast._id}
+        {...podcast}
+      />
+      <p className="text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center">
+        {podcast?.podcastDescription}
+      </p>
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-18 font-bold text-white-1">Transcription</h1>
+          <p className="text-16 font-medium text-white-2">
+            {podcast?.voicePrompt}
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-18 font-bold text-white-1">Thumbnail Prompt</h1>
+          <p className="text-16 font-medium text-white-2">
+            {podcast?.imagePrompt}
+          </p>
+        </div>
+      </div>
+      <section className="mt-8 flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">
+          Podcasts You May Like
+        </h1>
+        {similarPodcasts && similarPodcasts.length > 0 ? (
+          <div className="podcast_grid">
+            {similarPodcasts?.map(
+              ({ _id, podcastTitle, podcastDescription, imageUrl }) => (
+                <PodcastCard
+                  key={_id}
+                  imgUrl={imageUrl!}
+                  title={podcastTitle}
+                  description={podcastDescription}
+                  podcastId={_id}
+                />
+              )
+            )}
+          </div>
+        ) : (
+          <>
+            <EmptyState
+              title="No Similar Podcasts Found"
+              buttonLink="/discover"
+              buttonText="Discover More Podcasts"
+            />
+          </>
+        )}
+      </section>
     </section>
   );
 };

9893fd4fc8a22789d6cf0ba3e817c7713a180b4b
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-18 02:58:26 +0530

    Refactor LeftSidebar component to add dynamic profile link and improve UI

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -2,16 +2,26 @@
 
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
+import { SignedIn, SignedOut, useClerk, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { useAudio } from "@/providers/AudioProvider";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const { signOut } = useClerk();
+  const { user } = useUser();
+  const { audio } = useAudio();
 
   return (
-    <section className="left_sidebar">
+    <section
+      className={cn("left_sidebar h-[calc(100vh-5px)]", {
+        "h-[calc(100vh-140px)]": audio?.audioUrl,
+      })}
+    >
       <nav className="flex flex-col gap-6">
         <Link
           href={"/"}
@@ -41,7 +51,47 @@ const LeftSidebar = () => {
             </Link>
           );
         })}
+        {user && (
+          <Link
+            href={`/profile/${user.id}`}
+            className={cn(
+              "flex items-center justify-center gap-3 py-4 max-lg:px-4 lg:justify-start",
+              {
+                "bg-nav-focus border-r-4 border-orange-1":
+                  pathname === `/profile/${user.id}`,
+              }
+            )}
+          >
+            <Image
+              src="/icons/profile.svg"
+              alt="My Profile"
+              width={24}
+              height={24}
+            />
+            <p>My Profile</p>
+          </Link>
+        )}
       </nav>
+      <SignedOut>
+        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+          <Button
+            asChild
+            className="text-16 w-full bg-orange-1 font-extrabold rounded-md"
+          >
+            <Link href="/sign-in">Sign In</Link>
+          </Button>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+          <Button
+            onClick={() => signOut(() => router.push("/"))}
+            className="text-16 w-full bg-orange-1 font-extrabold rounded-md"
+          >
+            Sign Out
+          </Button>
+        </div>
+      </SignedIn>
     </section>
   );
 };

23e1137dd9402f0f46275e3c558a947940c32926
Menath Nadungoda <nadungmenath1@gmail.com>
2024-10-18 14:40:16 +0530

    Refactor home page to use EmptyState component for displaying no podcasts available

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,8 @@ import React from "react";
 import PodcastCard from "@/components/PodcastCard";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
-import { Mic } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import EmptyState from "@/components/EmptyState";
 
 const Home = () => {
   const trendingPodcasts = useQuery(api.podcasts.getTrendingPodcasts);
@@ -32,17 +31,11 @@ const Home = () => {
             )}
           </div>
         ) : (
-          <div className="flex flex-col items-center gap-3 p-4">
-            <Mic className="w-20 h-20 px-3 py-3 mb-2 rounded-full text-white-1 bg-orange-1" />
-            <p className="text-xl font-bold text-center text-white-1">
-              No podcasts available. Start creating your own podcast now!
-            </p>
-            <Link href={"/create-podcast"}>
-              <Button className="px-4 py-2 mt-4 font-bold rounded-md text-white-1 bg-orange-1 hover:bg-orange-500">
-                Create Podcast
-              </Button>
-            </Link>
-          </div>
+          <EmptyState
+            title="No podcasts available. Start creating your own podcast now!"
+            buttonLink="/create-podcast"
+            buttonText="Create Podcast"
+          />
         )}
       </section>
     </div>
